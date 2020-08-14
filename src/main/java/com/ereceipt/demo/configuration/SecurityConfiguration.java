b3c904808753dcf5224eb6b24b31b4d5f8fc1ec2
Ruslan Kononov <rukono@ttu.ee>
2020-08-14 21:19:21 +0300

    Add Spring Security configuration class

---
+++ src/main/java/com/ereceipt/demo/configuration/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.ereceipt.demo.configuration;
+
+import com.ereceipt.demo.security.MySimpleUrlAuthenticationSuccessHandler;
+import com.ereceipt.demo.security.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = MyUserDetailsService.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/doctor").hasAnyAuthority("DOCTOR")
+                .antMatchers("/pharma").hasAnyAuthority("PHARMA")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").successHandler(myAuthenticationSuccessHandler())
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordencoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MySimpleUrlAuthenticationSuccessHandler();
+    }
+}
