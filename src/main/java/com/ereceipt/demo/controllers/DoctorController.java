f927ed7b0884baa9aa8d2b341698117cfb212a8b
Ruslan Kononov <rukono@ttu.ee>
2020-08-21 12:16:52 +0300

    Add DoctorController and PharmaController

---
+++ src/main/java/com/ereceipt/demo/controllers/DoctorController.java
@@ -0,0 +1,51 @@
+package com.ereceipt.demo.controllers;
+
+import com.ereceipt.demo.domain.Doctor;
+import com.ereceipt.demo.domain.Prescription;
+import com.ereceipt.demo.service.DoctorService;
+import com.ereceipt.demo.service.PatientService;
+import net.bytebuddy.utility.RandomString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+
+@Controller
+@RequestMapping("doctor")
+public class DoctorController {
+
+    private final DoctorService doctorService;
+    private final PatientService patientService;
+
+    @Autowired
+    public DoctorController(DoctorService doctorService,PatientService patientService) {
+        this.doctorService = doctorService;
+        this.patientService = patientService;
+    }
+
+    @GetMapping("")
+    public String getAdminPage(Model model){
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Doctor doctor = doctorService.findDoctorByUsernameAndPassword(userDetails.getUsername(),
+                userDetails.getPassword());
+        model.addAttribute("doctor",doctor);
+        model.addAttribute("prescription",new Prescription());
+        model.addAttribute("patients",doctor.getPatients());
+        model.addAttribute("prescrCode", new RandomString().nextString());
+        return "doctor";
+    }
+
+    @PostMapping("/addPrescription")
+    public RedirectView addNewDoctor(@ModelAttribute("prescription") Prescription prescription, Model model){
+        doctorService.addNewPrescription(prescription);
+        return new RedirectView("/doctor",true);
+    }
+}
---
+++ src/main/java/com/ereceipt/demo/controllers/PharmaController.java
@@ -0,0 +1,64 @@
+package com.ereceipt.demo.controllers;
+
+import com.ereceipt.demo.domain.Pharmacist;
+import com.ereceipt.demo.domain.Prescription;
+import com.ereceipt.demo.dto.PrescriptionInfo;
+import com.ereceipt.demo.dto.PrescriptionNumber;
+import com.ereceipt.demo.service.PharmaService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("pharma")
+public class PharmaController {
+    private final PharmaService pharmaService;
+
+    @Autowired
+    public PharmaController(PharmaService pharmaService) {
+        this.pharmaService = pharmaService;
+    }
+
+    @GetMapping("")
+    public void getPharmaData(Model model){
+        UserDetails userDetails =
+                (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Pharmacist pharma = pharmaService.findPharmacistByUsernameAndPassword(userDetails.getUsername(),
+                userDetails.getPassword());
+        model.addAttribute("pharma",pharma);
+    }
+
+    @PostMapping(path = "/checkPrescription",consumes = "application/json", produces = "application/json")
+    public @ResponseBody String checkPrescription(@RequestBody PrescriptionNumber number){
+        Prescription prescription = pharmaService.findPrescriptionByItsCode(number.prescriptionNumber);
+        PrescriptionInfo info = new PrescriptionInfo();
+        if(prescription==null){
+            return new Gson().toJson("not found");
+        }
+        info.prescrId = prescription.getPrescrId().toString();
+        info.patientName = prescription.getPatient().getFirstName()+" "+prescription.getPatient().getLastName();
+        info.doctorName = prescription.getDoctor().getFirstName()+" "+prescription.getDoctor().getLastName();
+        info.medicine = prescription.getMedicineName();
+        info.date = prescription.getUtilDate().toString();
+        info.status = prescription.getHanded().toString();
+        String json = new Gson().toJson(info);
+        return json;
+    }
+
+    @PostMapping(path = "/issuePrescription",consumes = "application/json")
+    public @ResponseBody String issuePrescription(@RequestBody PrescriptionNumber number){
+        Prescription prescription = pharmaService.findPrescriptionById(Long.parseLong(number.prescriptionNumber));
+        if(!prescription.getHanded()){
+            prescription.setHanded(true);
+            pharmaService.savePrescription(prescription);
+            return new Gson().toJson("success");
+        }else{
+            return new Gson().toJson("already issued");
+        }
+    }
+}
