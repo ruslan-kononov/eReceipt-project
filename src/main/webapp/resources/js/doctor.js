b651d0357b359b4e81b027d386ea662d40019da5
Ruslan Kononov <rukono@ttu.ee>
2020-08-21 14:59:47 +0300

    Update validation mathods in doctor.js

---
+++ src/main/webapp/resources/js/doctor.js
@@ -0,0 +1,33 @@
+$('.btn-prescribe').click(function (){
+    if(validatePrescription()){
+        return true;
+    }else{
+        return false;
+    }
+});
+
+
+function validatePrescription() {
+    var patient = $(".patient");
+    var medicine = $(".medicine");
+    var prescrText = $(".prescrText");
+
+    var arr = [patient, medicine];
+
+    arr.forEach(el=>el.removeClass("is-invalid"))
+
+    if(patient.val()==""||medicine.val()==""){
+        jQuery.each(arr,function(index,value){
+            if(value.val() == ''){
+                value.addClass("is-invalid")
+            }
+        })
+        return false;
+    }else if(prescrText.val().length > 510) {
+        $(".prescrText").addClass("is-invalid");
+        return false;
+    }else{
+        return true;
+    }
+}
+
