7d852b8078d8300345f769982ef2d40cef521d44
Ruslan Kononov <rukono@ttu.ee>
2020-08-21 12:22:28 +0300

    Update PharmaService to handle prescription-related data

--- src/main/java/com/ereceipt/demo/service/PharmaService.java
+++ src/main/java/com/ereceipt/demo/service/PharmaService.java
@@ -1,17 +1,47 @@
 package com.ereceipt.demo.service;
 
 import com.ereceipt.demo.dao.PharmacistRepository;
+import com.ereceipt.demo.dao.PrescriptionRepository;
 import com.ereceipt.demo.domain.Pharmacist;
+import com.ereceipt.demo.domain.Prescription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PharmaService {
-    @Autowired
-    PharmacistRepository pharmacistRepository;
 
-    public Pharmacist findPharmacistByUsernameAndPassword(String username,String password){
+    private final PharmacistRepository pharmacistRepository;
+    private final PrescriptionRepository prescriptionRepository;
+
+    @Autowired
+    public PharmaService(PharmacistRepository pharmacistRepository,
+                         PrescriptionRepository prescriptionRepository) {
+        this.pharmacistRepository = pharmacistRepository;
+        this.prescriptionRepository = prescriptionRepository;
+    }
+
+    public Pharmacist findPharmacistByUsernameAndPassword(String username, String password){
         return pharmacistRepository.findPharmacistByUsernameAndPassword(username,password);
     }
+
+    public Prescription findPrescriptionByItsCode(String code){
+        return prescriptionRepository.findByPrescrCode(code);
+    }
+
+    public Prescription findPrescriptionById(Long id){
+        return prescriptionRepository.findByPrescrId(id);
+    }
+
+    public Prescription savePrescription(Prescription prescription){
+        return prescriptionRepository.save(prescription);
+    }
+
+    public Pharmacist addNewPharmacist(Pharmacist pharmacist){
+        return pharmacistRepository.save(pharmacist);
+    }
+
+
+
+
 
 }
