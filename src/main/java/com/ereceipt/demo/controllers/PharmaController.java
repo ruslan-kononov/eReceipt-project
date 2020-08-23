5890b7163d35bb2e8dd6abe93c95c16d92409a9f
Ruslan Kononov <rukono@ttu.ee>
2020-08-23 15:23:41 +0300

    Add method to retrieve image from DB by id

--- src/main/java/com/ereceipt/demo/controllers/PharmaController.java
+++ src/main/java/com/ereceipt/demo/controllers/PharmaController.java
@@ -1,12 +1,15 @@
 package com.ereceipt.demo.controllers;
 
+import com.ereceipt.demo.domain.FileMultipart;
 import com.ereceipt.demo.domain.Pharmacist;
 import com.ereceipt.demo.domain.Prescription;
 import com.ereceipt.demo.dto.PrescriptionInfo;
 import com.ereceipt.demo.dto.PrescriptionNumber;
+import com.ereceipt.demo.service.FileMultipartService;
 import com.ereceipt.demo.service.PharmaService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,14 +17,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
 @Controller
 @RequestMapping("pharma")
 public class PharmaController {
     private final PharmaService pharmaService;
+    private final FileMultipartService fileMultipartService;
 
     @Autowired
-    public PharmaController(PharmaService pharmaService) {
+    public PharmaController(PharmaService pharmaService, FileMultipartService fileMultipartService) {
         this.pharmaService = pharmaService;
+        this.fileMultipartService = fileMultipartService;
     }
 
     @GetMapping("")
@@ -31,6 +42,15 @@ public class PharmaController {
         Pharmacist pharma = pharmaService.findPharmacistByUsernameAndPassword(userDetails.getUsername(),
                 userDetails.getPassword());
         model.addAttribute("pharma",pharma);
+    }
+
+    @GetMapping("/getPhoto/{fileId}")
+    public void downlaodFile(@PathVariable UUID fileId, HttpServletResponse response) throws IOException {
+        FileMultipart fileMultipart = fileMultipartService.getFile(fileId);
+        response.setContentType("image/jpeg");
+        byte[] bytes = fileMultipart.getData();
+        InputStream inputStream = new ByteArrayInputStream(bytes);
+        IOUtils.copy(inputStream, response.getOutputStream());
     }
 
     @PostMapping(path = "/checkPrescription",consumes = "application/json", produces = "application/json")
