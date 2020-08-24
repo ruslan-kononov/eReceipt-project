5f816eae4465fc222412e1f4eacc917f2e97f3d3
Ruslan Kononov <rukono@ttu.ee>
2020-08-24 16:22:13 +0300

    Add method to display seetings page and method to update profile's photo

--- src/main/java/com/ereceipt/demo/controllers/AdminController.java
+++ src/main/java/com/ereceipt/demo/controllers/AdminController.java
@@ -2,10 +2,12 @@ package com.ereceipt.demo.controllers;
 
 import com.ereceipt.demo.domain.*;
 import com.ereceipt.demo.service.*;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
